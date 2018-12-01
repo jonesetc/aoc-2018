318ae5560b030815526b5370ebefb3597e59a882
Kyle Jones <kyle@jonesetc.com>
2018-12-01 18:03:56 -0500

    day 1 and some structure

---
+++ day1/src/bin/part1.rs
@@ -0,0 +1,33 @@
+const INPUT: &str = include_str!("../../etc/input-part1.txt");
+
+fn main() {
+    println!("answer is: {}", process(INPUT).to_string())
+}
+
+fn process(input: &str) -> impl ToString {
+    input
+        .trim()
+        .split('\n')
+        .map(|line| line.parse::<i32>().unwrap())
+        .sum::<i32>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::process;
+
+    #[test]
+    fn example1() {
+        assert_eq!(process(&"+1, +1, +1".replace(", ", "\n")).to_string(), "3");
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(process(&"+1, +1, -2".replace(", ", "\n")).to_string(), "0");
+    }
+
+    #[test]
+    fn example3() {
+        assert_eq!(process(&"-1, -2, -3".replace(", ", "\n")).to_string(), "-6");
+    }
+}
---
+++ day1/src/bin/part2.rs
@@ -0,0 +1,60 @@
+use std::collections;
+use std::iter;
+
+const INPUT: &str = include_str!("../../etc/input-part2.txt");
+
+fn main() {
+    println!("answer is: {}", process(INPUT).to_string())
+}
+
+fn process(input: &str) -> impl ToString {
+    let mut current_freq: i32 = 0;
+    let mut previous_freqs: collections::HashSet<i32> = [current_freq].iter().cloned().collect();
+
+    for value in iter::repeat(input.trim()).flat_map(|input| {
+        input
+            .split('\n')
+            .map(|line| line.trim().parse::<i32>().unwrap())
+    }) {
+        current_freq += value;
+        if !previous_freqs.insert(current_freq) {
+            break;
+        }
+    }
+
+    current_freq
+}
+
+#[cfg(test)]
+mod tests {
+    use super::process;
+
+    #[test]
+    fn example1() {
+        assert_eq!(process(&"+1, -1".replace(", ", "\n")).to_string(), "0");
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(
+            process(&"+3, +3, +4, -2, -4".replace(", ", "\n")).to_string(),
+            "10"
+        );
+    }
+
+    #[test]
+    fn example3() {
+        assert_eq!(
+            process(&"-6, +3, +8, +5, -6".replace(", ", "\n")).to_string(),
+            "5"
+        );
+    }
+
+    #[test]
+    fn example4() {
+        assert_eq!(
+            process(&"+7, +7, -2, -7, -4".replace(", ", "\n")).to_string(),
+            "14"
+        );
+    }
+}
