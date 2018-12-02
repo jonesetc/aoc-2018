836e0d335064817ecc55f8811bf0d49865421431
Kyle Jones <kyle@jonesetc.com>
2018-12-02 18:16:30 -0500

    day2

---
+++ day2/src/bin/part1.rs
@@ -0,0 +1,45 @@
+use std::collections::{HashMap, HashSet};
+
+const INPUT: &str = include_str!("../../etc/input-part1.txt");
+
+fn main() {
+    println!("answer is: {}", process(INPUT).to_string())
+}
+
+fn process(input: &str) -> impl ToString {
+    let mut doubles: u32 = 0;
+    let mut triples: u32 = 0;
+
+    for line in input.lines() {
+        let mut counters: HashMap<char, u32> = HashMap::new();
+
+        for c in line.chars() {
+            *counters.entry(c).or_insert(0) += 1;
+        }
+
+        let unique_tallies: HashSet<&u32> = counters.values().collect();
+        if unique_tallies.contains(&2) {
+            doubles += 1;
+        }
+        if unique_tallies.contains(&3) {
+            triples += 1;
+        }
+    }
+    doubles * triples
+}
+
+#[cfg(test)]
+mod tests {
+    use super::process;
+
+    #[test]
+    fn example1() {
+        assert_eq!(
+            process(
+                &vec!["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]
+                    .join("\n")
+            ).to_string(),
+            "12"
+        );
+    }
+}
---
+++ day2/src/bin/part2.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+
+const INPUT: &str = include_str!("../../etc/input-part2.txt");
+
+fn main() {
+    println!("answer is: {}", process(INPUT).to_string())
+}
+
+fn process(input: &str) -> impl ToString {
+    let mut previous_ids: HashSet<(&str, &str)> = HashSet::new();
+
+    for line in input.lines() {
+        for i in { 0..line.len() } {
+            let masked_id: (&str, &str) = (&line[..i], &line[i + 1..]);
+            if !previous_ids.insert(masked_id) {
+                return format!("{}{}", masked_id.0, masked_id.1);
+            }
+        }
+    }
+    panic!("something went wrong")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::process;
+
+    #[test]
+    fn example1() {
+        assert_eq!(
+            process(
+                &vec!["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"].join("\n")
+            ).to_string(),
+            "fgij"
+        );
+    }
+}
